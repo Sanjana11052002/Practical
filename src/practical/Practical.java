bd700418f3d096481300de9b1e4c7147d94e572c
sanjana <sanjana5@sheridancollege.ca>
2024-02-21 12:03:57 -0500

    third Commit

--- src/practical/Practical.java
+++ src/practical/Practical.java
@@ -15,7 +15,7 @@ public class Practical {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HelloWorld")
+        System.out.println("HelloWorld");
     }
     
 }
